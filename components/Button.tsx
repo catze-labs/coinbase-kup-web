3e275bba66e2b187de4f3b093e054d576162cf69
jamie-jungah <wjddk718@gmail.com>
2023-01-04 16:31:16 +0900

    Fix a property of the loading icon

--- components/Button.tsx
+++ components/Button.tsx
@@ -18,7 +18,7 @@ const LoadingIcon = () => (
     >
       <circle cx="12.5" cy="12.5" r="12.5">
         <animate
-          attributeName="fill-opacity"
+          attributeName="fillOpacity"
           begin="0s"
           dur="1s"
           values="1;.2;1"
@@ -26,9 +26,9 @@ const LoadingIcon = () => (
           repeatCount="indefinite"
         />
       </circle>
-      <circle cx="12.5" cy="52.5" r="12.5" fill-opacity=".5">
+      <circle cx="12.5" cy="52.5" r="12.5" fillOpacity=".5">
         <animate
-          attributeName="fill-opacity"
+          attributeName="fillOpacity"
           begin="100ms"
           dur="1s"
           values="1;.2;1"
@@ -38,7 +38,7 @@ const LoadingIcon = () => (
       </circle>
       <circle cx="52.5" cy="12.5" r="12.5">
         <animate
-          attributeName="fill-opacity"
+          attributeName="fillOpacity"
           begin="300ms"
           dur="1s"
           values="1;.2;1"
@@ -48,7 +48,7 @@ const LoadingIcon = () => (
       </circle>
       <circle cx="52.5" cy="52.5" r="12.5">
         <animate
-          attributeName="fill-opacity"
+          attributeName="fillOpacity"
           begin="600ms"
           dur="1s"
           values="1;.2;1"
@@ -58,7 +58,7 @@ const LoadingIcon = () => (
       </circle>
       <circle cx="92.5" cy="12.5" r="12.5">
         <animate
-          attributeName="fill-opacity"
+          attributeName="fillOpacity"
           begin="800ms"
           dur="1s"
           values="1;.2;1"
@@ -68,7 +68,7 @@ const LoadingIcon = () => (
       </circle>
       <circle cx="92.5" cy="52.5" r="12.5">
         <animate
-          attributeName="fill-opacity"
+          attributeName="fillOpacity"
           begin="400ms"
           dur="1s"
           values="1;.2;1"
@@ -78,7 +78,7 @@ const LoadingIcon = () => (
       </circle>
       <circle cx="12.5" cy="92.5" r="12.5">
         <animate
-          attributeName="fill-opacity"
+          attributeName="fillOpacity"
           begin="700ms"
           dur="1s"
           values="1;.2;1"
@@ -88,7 +88,7 @@ const LoadingIcon = () => (
       </circle>
       <circle cx="52.5" cy="92.5" r="12.5">
         <animate
-          attributeName="fill-opacity"
+          attributeName="fillOpacity"
           begin="500ms"
           dur="1s"
           values="1;.2;1"
@@ -98,7 +98,7 @@ const LoadingIcon = () => (
       </circle>
       <circle cx="92.5" cy="92.5" r="12.5">
         <animate
-          attributeName="fill-opacity"
+          attributeName="fillOpacity"
           begin="200ms"
           dur="1s"
           values="1;.2;1"
